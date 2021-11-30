feb283507f814fecdb81dcb75dd8471dd5031a22
wahyudioputra <wdp41124@gmail.com>
2021-12-01 04:57:04 +0700

    Create by wahyudioputra

---
+++ functions/menu.js
@@ -0,0 +1,326 @@
+exports.menu = (v) => {
+  return `
+
+‣ ${v}info
+‣ ${v}information
+to view information
+
+‣ ${v}bahasa
+‣ ${v}language
+to change language 🇬🇧🔁🇮🇩
+
+‣ ${v}ping
+‣ ${v}speed
+see robot speed
+
+‣ ${v}report
+‣ ${v}lapor
+if something is broken you can report it to the owner
+
+‣ ${v}chatbot on/off
+to reply to each other's messages of questions and answers
+
+‣ ${v}dilan
+dilan wise words
+there is no translation, this feature is only Indonesian
+
+‣ ${v}ilham
+inspirational wise words
+there is no translation, this feature is only Indonesian
+
+‣ ${v}gombal
+crap word
+there is no translation, this feature is only Indonesian
+
+‣ ${v}hacker
+anonymous wise words
+there is no translation, this feature is only Indonesian
+
+‣ ${v}fakta
+fact words
+there is no translation, this feature is only Indonesian
+
+‣ ${v}apakah
+what question
+there is no translation, this feature is only Indonesian
+
+‣ ${v}kapankah
+question when
+there is no translation, this feature is only Indonesian
+
+‣ ${v}siapakah
+whose question?
+there is no translation, this feature is only Indonesian
+
+‣ ${v}truth
+word of truth
+there is no translation, this feature is only Indonesian
+
+‣ ${v}dare
+bold words
+there is no translation, this feature is only Indonesian
+
+‣ ${v}asahotak
+brain teasers
+there is no translation, this feature is only Indonesian
+
+‣ ${v}siapakahaku
+who am i game
+there is no translation, this feature is only Indonesian
+
+‣ ${v}susunkata
+word game
+there is no translation, this feature is only Indonesian
+
+‣ ${v}tebakkalimat
+guessing game
+there is no translation, this feature is only Indonesian
+
+‣ ${v}tebakkata
+guessing game
+there is no translation, this feature is only Indonesian
+
+‣ ${v}tebaklirik
+lyrics guessing game
+there is no translation, this feature is only Indonesian
+
+‣ ${v}tekateki
+puzzle game
+there is no translation, this feature is only Indonesian
+
+‣ ${v}suit gunting/batu/kertas
+rock paper scissors game
+there is no translation, this feature is only Indonesian
+
+‣ ${v}sticker
+make stickers
+
+‣ ${v}toimg
+take your sticker and make a big picture examples: #toimg (image)
+
+‣ ${v}pinterest <query>
+search for pictures on pinterest
+
+‣ ${v}cogan
+boy pictures
+see handsome boy
+
+‣ ${v}cecan
+girl pictures
+see beautiful girl
+
+‣ ${v}cyberpunk
+cyberpunk pictures
+
+‣ ${v}wallpaper
+looking for wallpapers
+
+‣ ${v}afk <reason>
+afk features
+
+‣ ${v}fakethumb
+fake thumb feature
+
+‣ ${v}tomp3
+take your video and make it mp3
+
+‣ ${v}tovn
+take your video and make vn
+
+‣ ${v}delete
+delete what has been done
+
+‣ ${v}covid
+see how many people die from covid
+
+‣ ${v}translate <lang><text>
+this feature can translate Indonesian and English, for
+example #translate id are you okay?
+
+‣ ${v}artinama <query>
+‣ ${v}name <query>
+name meaning feature
+your name mention example #name revelation
+
+‣ ${v}artimimpi <query>
+‣ ${v}dream
+dream meaning feature
+what happened in your dreams, and look for examples of #dream nightmares
+
+‣ ${v}playstore <query>
+search without opening play store
+this feature is looking for an example application of #playstore termux
+
+‣ ${v}ytsearch <query>
+search without opening youtube
+how to function example #ytsearch learn
+
+‣ ${v}cerpen
+short story
+This feature is only for Indonesia
+
+‣ ${v}google <query>
+‣ ${v}brainly <query>
+search without opening google
+
+‣ ${v}shortlink <link>
+how to run this feature example #shortlink <link>
+function of this feature, change your link to tinyurl
+
+‣ ${v}igdl <link>
+download videos on instagram
+how to run this feature example #igdl <link>
+
+‣ ${v}igvid <link>
+download videos on instagram
+how to run this feature example #igvid <link>
+
+‣ ${v}igimg <link>
+download pictures on instagram
+how to run this feature example #igimg <link>
+
+‣ ${v}reels <link>
+this feature is useful instagram link
+how to run this feature example #reels <link>
+
+‣ ${v}tiktok <link>
+this feature is useful tiktok link
+how to run this feature example #tiktok <link>
+
+‣ ${v}tiktoknowm <link>
+this feature is useful for removing the name of tiktok
+how to run this feature example #tiktoknowm <link>
+
+‣ ${v}tiktokaudio <link>
+this feature to make mp3
+how to run this feature example #tiktokaudio <link>
+
+‣ ${v}joox <query>
+search for songs on joox
+how to run this feature example #joox hero
+
+‣ ${v}play <query>
+search for songs on play
+how to run this feature example #play hero
+
+‣ ${v}ytmp3 <link>
+pick up song
+how to run this feature example #ytmp4 <link>
+
+‣ ${v}ytmp4 <link>
+take video
+how to run this feature example #ytmp4 <link>
+
+‣ ${v}open
+open group
+
+‣ ${v}close
+close group
+
+‣ ${v}add <number>
+add member
+
+‣ ${v}kick @user
+kick members
+
+‣ ${v}warn @user
+give a warning
+
+‣ ${v}delwarn @user
+remove warning
+
+‣ ${v}cekwarn @user
+check alerts
+
+</VN
+‣ ${v}fast
+‣ ${v}slow
+‣ ${v}reverse
+‣ ${v}gemuk
+‣ ${v}tupai
+‣ ${v}bass
+‣ ${v}nightcore
+this feature change your voice to be good, tag your vn and type #fast
+
+</DATA BASE
+‣ ${v}addimg <text>
+‣ ${v}addvid <text>
+‣ ${v}addstik <text>
+‣ ${v}addvn <text>
+‣ ${v}getimg <text>
+‣ ${v}getvid <text>
+‣ ${v}getstik <text>
+‣ ${v}getvn <text>
+‣ ${v}listmedia
+
+</SECURITY GROUP
+‣ ${v}welcome on
+‣ ${v}welcome off
+‣ ${v}antidelete on
+‣ ${v}antidelete off
+‣ ${v}detect on
+‣ ${v}detect off
+‣ ${v}antilink on
+‣ ${v}antilink off
+‣ ${v}antiviewonce on
+‣ ${v}antiviewonce off
+
+</GROUP
+‣ ${v}open
+‣ ${v}close
+‣ ${v}link
+‣ ${v}revoke
+‣ ${v}online
+‣ ${v}offline
+‣ ${v}hidetag
+‣ ${v}setppgc
+‣ ${v}setname <text>
+‣ ${v}setdesk <text>
+‣ ${v}profile @tag
+‣ ${v}infogroup
+‣ ${v}add <nomor>
+‣ ${v}kick @user
+‣ ${v}promote @user
+‣ ${v}demote @user
+‣ ${v}warn @user
+‣ ${v}delwarn @user
+‣ ${v}cekwarn @user
+‣ ${v}setwelcome <text>
+‣ ${v}setbye <text>
+‣ ${v}delwelcome
+‣ ${v}delbye
+‣ ${v}simulasi welcome
+‣ ${v}simulasi bye
+ 
+</OWNER
+‣ ${v}join <link>
+‣ ${v}grouplist
+‣ ${v}addpremium @user
+‣ ${v}delpremium @user
+‣ ${v}banned @user
+‣ ${v}unbanned @user
+‣ ${v}upsw
+‣ ${v}setppbot
+‣ ${v}setthumb
+‣ ${v}setfakethumb
+‣ ${v}onepref
+‣ ${v}nopref
+‣ ${v}multi
+‣ ${v}view <link>
+‣ ${v}inspect <link>
+‣ ${v}listreport
+‣ ${v}listprem
+‣ ${v}listban
+‣ $
+  >
+  
+  
+🇬🇧
+if there is a feature error, immediately report it to me by #report reason
+
+
+🇮🇩
+fitur bot error? lapor dengan cara #lapor (alasan)
+
+Developer: @wahyudioputra`; // jangan di hapus!!
+};
